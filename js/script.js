3b05b0995df71389933c9e7ee2cb0528ebdc9139
dandyrizkifirdaus <rizkifirdausdandy@gmail.comm>
2025-03-09 17:26:05 +0700

    Update Dikit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Ini Javascript
 function greetUser() {
     let name = prompt("Masukkan nama Anda:");
-    let greetingText = name ? `Hi, ${name}! Selamat datang di website saya.` : "Hi, Pengunjung! Selamat datang di website saya.";
+    let greetingText = name ? `Hi, ${name}! Selamat datang di Website Aku.` : "Hi, Pengunjung! Selamat datang di Website Aku.";
     document.getElementById("greeting").innerText = greetingText;
 }
 
