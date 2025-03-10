8be5d677495e5461d53f3b083859476f412fb3ef
dandyrizkifirdaus <rizkifirdausdandy@gmail.comm>
2025-03-10 16:23:41 +0700

    Update Terakhir

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 // Ini Javascript
 function greetUser() {
-    let name = prompt("Masukkan nama Anda:");
+    let name = prompt("Hi,Pengunjung Siapa Namamu?");
     let greetingText = name ? `Hi, ${name}! Selamat datang di Website Aku.` : "Hi, Pengunjung! Selamat datang di Website Aku.";
     document.getElementById("greeting").innerText = greetingText;
 }
 
 // Panggil fungsi saat halaman selesai dimuat
 window.onload = greetUser;
+
+// massage us
+document.getElementById('submit').addEventListener('click', function() 
+// Mengambil nilai dari input
+{   var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    var phone = document.getElementById('phone').value;
+
+    // Menampilkan hasil di bagian result
+    document.getElementById('displayName').innerText = "Nama: " + name;
+    document.getElementById('displayEmail').innerText = "Email: " + email;
+    document.getElementById('displayPhone').innerHTML = "Nomor Telepon: " + phone;
+    document.getElementById('displayMessage').innerText = "Pesan: " + message;
+
+
+    // Menampilkan div result
+    document.getElementById('result').style.display = 'block';
+});
