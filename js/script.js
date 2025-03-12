1713193d3ee9e9ccaa6dc5cb5e82b0e59b6a256b
dandyrizkifirdaus <rizkifirdausdandy@gmail.comm>
2025-03-12 15:06:58 +0700

    Update Terakhir

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ window.onload = greetUser;
 
 // massage us
 document.getElementById('submit').addEventListener('click', function() 
+
 // Mengambil nilai dari input
 {   var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
@@ -26,3 +27,30 @@ document.getElementById('submit').addEventListener('click', function()
     // Menampilkan div result
     document.getElementById('result').style.display = 'block';
 });
+
+// Slide show
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var slides = document.getElementsByClassName("lecture-img");
+    
+    if (n > slides.length) {
+        slideIndex = 1; // Kembali ke slide pertama jika melebihi jumlah slide
+    }
+    if (n < 1) {
+        slideIndex = slides.length; // Kembali ke slide terakhir jika kurang dari 1
+    }
+
+    // Sembunyikan semua slide
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Tampilkan slide yang aktif
+    slides[slideIndex - 1].style.display = "block";
+}
